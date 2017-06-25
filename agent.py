90564b13a50f7baa3f2aa7302c95c76b5e1c039e
Sathish A <sathish97rockz@gmail.com>
2017-06-25 12:09:30 +0530

    Final

--- agent.py
+++ agent.py
@@ -20,26 +20,22 @@ for lab in label1:
 
 dataframe = dataframe.drop('class', 1)
 
-dataframe.replace('?',-99999, inplace=True)
+dataframe.replace('?', -99999, inplace=True)
 data = np.array(dataframe)
 label = np.array(label)
-
 
 model = Sequential()
 model.add(Dense(48, input_dim=9, activation='tanh'))
 model.add(Dense(16, activation='tanh'))
 model.add(Dense(2, activation='softmax'))
-tbCallBack = keras.callbacks.TensorBoard(log_dir='/tmp/tflearn_logs/', histogram_freq=0, write_graph=True, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
+tbCallBack = keras.callbacks.TensorBoard(log_dir='/tmp/tflearn_logs/', histogram_freq=0, write_graph=True,
+                                         write_images=False, embeddings_freq=0, embeddings_layer_names=None,
+                                         embeddings_metadata=None)
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 model.fit(data, label, epochs=1000, batch_size=70, validation_data=(data, label), callbacks=[tbCallBack])
 score = model.evaluate(data, label, batch_size=100)
 
 print(score)
 
-
-test = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,1,2,3,2,1]])
+test = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 1, 1, 1, 2, 3, 2, 1]])
 print(model.predict(test))
-
-
-#0.93991416360175661 accuracy
-#loss 0.078175581895742294
